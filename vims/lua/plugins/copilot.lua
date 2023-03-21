760fec8b526759bd1588092cc041d09f06bf1109
matatratata <ulubionezwierze@gmail.com>
2023-03-21 12:11:31 +0100

    vims

---
+++ vims/lua/plugins/copilot.lua
@@ -0,0 +1,18 @@
+return {
+	"zbirenbaum/copilot.lua",
+	cmd = "Copilot",
+	event = "InsertEnter",
+	config = function()
+		require("copilot").setup({
+			suggestion = { enabled = false },
+			panel = { enabled = false },
+			filetypes = {
+				lua = true,
+				python = true,
+				markdown = true,
+				zsh = true,
+				toml = true,
+			},
+		})
+	end,
+}
